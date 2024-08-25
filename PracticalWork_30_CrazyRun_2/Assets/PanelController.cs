f3df1cf833e25e5cdc238bc4681db363aab3b7fb
finashin82 <finashin82@mail.ru>
2024-08-26 08:44:47 +0900

    Начал делать Главное меню
    Начал делать Главное меню

---
+++ PracticalWork_30_CrazyRun_2/Assets/PanelController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameOverPanel;
+
+    [SerializeField] private GameObject _winPanel;
+
+    public void GameOverPanel(Timer timer)
+    {
+        if (!timer.IsTimeForGame)
+        {
+            Time.timeScale = 0;
+
+            _gameOverPanel.SetActive(true);
+        }        
+    }
+
+    public void WinPanel()
+    {
+        Time.timeScale = 0;
+
+        _winPanel.SetActive(true);
+    }
+}
